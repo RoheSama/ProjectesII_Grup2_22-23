93f88c7086b5c1a47b5eeb48551e73243c2a1889
RoheSama <roger.companys@enti.cat>
2022-11-04 19:54:01 +0100

    Fixed Collisions

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,14 +7,11 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     public float followSpeed = 2f;
     public Transform target;
-   
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y, -10);
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
         transform.position = Vector3.Slerp(transform.position, newPos, followSpeed * Time.deltaTime);
-
-
     }
 }
