eadda187f60f394b9fda21b3e74d4162f26a41af
RoheSama <roger.companys@enti.cat>
2022-11-06 19:07:28 +0100

    New voids movement

---
+++ Assets/Scripts/Voids/VoidController.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class VoidController : MonoBehaviour
+{
+    private int decisionClock = 0;
+    private int decisionTime = 220;
+
+    private Vector2 direction = Vector2.zero;
+
+    private float movePower = 350.4f;
+
+    private Animator anim;
+    private Rigidbody2D rb;
+
+    private int orientation = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Tick();
+        Move();
+    }
+
+    private void Move()
+    {
+        //Run animation
+        anim.SetBool("isRun", direction.sqrMagnitude > 0.0001f);
+
+        //Set animator direction
+        if (direction.x > 0.0001f)
+            orientation = 1;
+        else if (direction.x < -0.0001f)
+            orientation = -1;
+
+        transform.localScale = new Vector3(orientation, 1, 1);
+
+        rb.AddForce(direction * movePower * Time.fixedDeltaTime, ForceMode2D.Force);
+    }
+    private void Tick()
+    {
+        decisionClock++;
+        if(decisionClock > decisionTime)
+        {
+            decisionClock = 0;
+
+            direction = GetDirection();
+            
+
+        }
+    }
+
+    private Vector2 GetDirection()
+    {
+        System.Random random = new System.Random();
+
+        Vector3 newDirection = new Vector3(random.Next(-1, 2), random.Next(-1, 2), 0);
+
+        return ToIso(newDirection.normalized);
+
+    }
+
+    private Vector3 ToIso(Vector3 cartesianVector)
+    {
+        return new Vector3(cartesianVector.x - cartesianVector.y,
+            (cartesianVector.x + cartesianVector.y) / 2,0);
+    }
+}
