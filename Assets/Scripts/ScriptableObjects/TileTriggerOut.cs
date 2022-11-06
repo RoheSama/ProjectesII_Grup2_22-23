aa06f244c388cbd983f0c86fd63f79583326328e
Emma <emma.polanco@enti.cat>
2022-11-06 15:24:09 +0100

    Trigger Scripts Working
    Next commit will have all triggers on map

--- Assets/Scripts/ScriptableObjects/TileTriggerOut.cs
+++ Assets/Scripts/ScriptableObjects/TileTriggerOut.cs
@@ -3,25 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-
-public class TileTrigger : MonoBehaviour
+public class TileTriggerOut : MonoBehaviour
 {
     public Collider2D col;
     public Tilemap tilemap;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        tilemap.color = new Color(tilemap.color.r, tilemap.color.g, tilemap.color.b, 0.25f);
+        tilemap.color = new Color(tilemap.color.r, tilemap.color.g, tilemap.color.b, 1f);
     }
 }
