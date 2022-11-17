fa21350dc4334cf768d7229e66a600b28ffa783a
Arnau Pradas Soriano <arnau.pradas@enti.cat>
2022-11-17 20:30:47 +0100

    Changes

--- Assets/Scripts/Phone/MessageApp.cs
+++ Assets/Scripts/Phone/MessageApp.cs
@@ -5,28 +5,24 @@ using UnityEngine.UI;
 
 public class MessageApp : MonoBehaviour
 {
-    public Image[] textGap;
+    public Sprite[] textGap;
 
-    public Image lastText;
+    public Sprite lastText;
 
 void Start()
 {
     for(int i = 0; i < textGap.Length; i++)
     {
         textGap[i].GetComponent<Image>();
-            textGap[i] = null;
+        textGap[i] = null;
     }
 
-    for (int i = 0; i < textGap.Length; i++)
-    {
-       // textGap[i].color = new Color(1f, 1f, 1f, 0f);
-    }
-    
      for(int i = 0; i < textGap.Length; i++)
      {
             if (textGap[i] == null)
             {
-                textGap[i] = lastText;
+                Debug.Log("a");
+                textGap[i] = lasText;
             }
      }
 
