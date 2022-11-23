afcdb72cddbb37b3a19597974f0a7b6049ec07f8
Arnau Pradas Soriano <arnau.pradas@enti.cat>
2022-11-23 20:02:18 +0100

    gitflow-feature-stash: Abilities

---
+++ Assets/Project/Scripts/New Scripts/DashAbility.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class DashAbility : Ability
+{
+
+    public float dashVelocity;
+
+    public override void Activate(GameObject parent) 
+    {
+        Debug.Log("a");
+    }
+    
+    
+    
+}
