6fcfac52854ce39e6cd5b5d96e2405517a9833b9
Emma <emma.polanco@enti.cat>
2023-01-06 13:27:18 +0100

    Revert "Merge branch 'feature/Tutorial2' into develop"
    This reverts commit bb112780e21e732045c16cb63eb738d325914fbe.

--- Assets/Project/Scripts/Player/CameraController.cs
+++ Assets/Project/Scripts/Player/CameraController.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    // Start is called before the first frame update
+    public float followSpeed = 2f;
     public Transform target;
 
     public float duration = 0.5f;
@@ -15,8 +17,10 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(target.transform.position.x, 
-            target.transform.position.y, transform.position.z);
+        Vector3 targetPos = (Vector3)target.GetComponent<Rigidbody2D>().velocity * 0.1f + target.position + Vector3.up;
+        targetPos.z = -10f;
+        Vector3 newPos = new Vector3(targetPos.x, targetPos.y, -10f);
+        transform.position = Vector3.Slerp(targetPos, newPos, followSpeed * Time.deltaTime);
 
         if (start)
         {
