a3ad71935b649fc4c2720e6584e0031e50a21fb6
Arnau Pradas Soriano <arnau.pradas@enti.cat>
2023-03-06 19:26:25 +0100

    Cross Layer Fixed

--- Assets/CrossEffect.cs
+++ Assets/CrossEffect.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class CrossEffect : MonoBehaviour
 {
     public TopDownMovement topDownMovement;
+    public GameObject crossEnabledRange;
     public Animator animator;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("CrossEnabledRange"))
         {
-            Debug.Log("AFECTADO POR LA CRUZ");
             topDownMovement.shadowSpeed = 1;
-            animator=other.GetComponent<Animator>();
+            crossEnabledRange = other.gameObject;
+            animator = crossEnabledRange.transform.parent.GetComponent<Animator>();
             animator.SetBool("CanShine", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("CrossEnabledRange"))
+        {
+            crossEnabledRange = other.gameObject;
+            animator = crossEnabledRange.transform.parent.GetComponent<Animator>();
+            animator.SetBool("CanShine", false);
         }
     }
 }
