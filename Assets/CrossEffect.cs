0f7db41c996843b27760e84ab68870abec063003
Arnau Pradas Soriano <arnau.pradas@enti.cat>
2023-03-06 17:40:18 +0100

    Cross Works

--- Assets/CrossEffect.cs
+++ Assets/CrossEffect.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class CrossEffect : MonoBehaviour
 {
+    public TopDownMovement topDownMovement;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("CrossEnabledRange"))
         {
             Debug.Log("AFECTADO POR LA CRUZ");
+            topDownMovement.shadowSpeed = 1;
+
         }
     }
 }
