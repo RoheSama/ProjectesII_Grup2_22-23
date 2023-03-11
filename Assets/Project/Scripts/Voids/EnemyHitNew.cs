15086eebe7ff3fff7a10ef0ea20fe9d9461717b2
RoheSama <roger.companys@enti.cat>
2023-03-11 18:25:10 +0100

    Tutorial Crosses fixed

--- Assets/Project/Scripts/Voids/EnemyHitNew.cs
+++ Assets/Project/Scripts/Voids/EnemyHitNew.cs
@@ -100,7 +100,7 @@ public class EnemyHitNew : MonoBehaviour
     {
 
         died = true;
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(1.0f);
         Destroy(gameObject);
         if (isCura)
         {
