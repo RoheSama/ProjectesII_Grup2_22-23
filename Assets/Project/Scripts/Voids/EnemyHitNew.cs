9a483b7ee727018bb997cbfb0c174443e1d0aa98
Emma <emma.polanco@enti.cat>
2023-03-12 18:50:15 +0100

    CuraDeath Sound Fixed
    He does not sound like a girl anymore

--- Assets/Project/Scripts/Voids/EnemyHitNew.cs
+++ Assets/Project/Scripts/Voids/EnemyHitNew.cs
@@ -81,13 +81,14 @@ public class EnemyHitNew : MonoBehaviour
     void Die()
     {
         //Debug.Log("Enemy died");
-        FindObjectOfType<AudioManager>().Play("DeathVoid");
+        FindObjectOfType<AudioManager>().Play("CuraDeathSound");
         anim.SetBool("IsDead", true);
         GetComponent<Collider2D>().enabled = false;
         agent.speed = 0.0f;
         StartCoroutine(VoidDestroy());
         if (!isCura)
         {
+            FindObjectOfType<AudioManager>().Play("DeathVoid");
             voidsLeft.GetComponent<VoidsLeft>().studentKill();
             iABehaviour.isDead = true;
         }
